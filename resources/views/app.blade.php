68c78b77345e916d4a960960e2812c20e48653a5
ahmadrosid <alahmadrosid@gmail.com>
2023-05-17 22:36:27 +0700

    chore: tidy up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Inter:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=Inter:400,500,600,800,900&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
     @routes
