bcb84c4722a436d7b32c695cecf63c5cb69c0917
ahmadrosid <alahmadrosid@gmail.com>
2023-05-18 00:04:42 +0700

    chore: tidy up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'PDFPINTAR') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
