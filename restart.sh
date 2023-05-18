d4ed34580ab21ec29cc9f5630d56041ea19466a5
ahmadrosid <alahmadrosid@gmail.com>
2023-05-18 13:37:45 +0700

    chore: tidy up

--- restart.sh
+++ restart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Restarting php8.1-fpm."
 sudo systemctl restart php8.1-fpm
+
+echo "Restarting NGINX."
 sudo systemctl restart nginx
+
+echo "Restarting queue."
 sudo supervisorctl restart all
