e1fd13d735eb9b91186e2f81609ae1fcf740b98c
caesar003 <caesarmuksid@gmail.com>
2023-10-07 14:27:41 +0700

    fix: removed arrow icon on button prompt

--- resources/js/Pages/Documents/Chat/Show.tsx
+++ resources/js/Pages/Documents/Chat/Show.tsx
@@ -50,7 +50,7 @@ const StreamingMessage = forwardRef<ElementRef<"span">, StreamingMessageProps>(
         <div
             className={clsx(
                 !show && "hidden",
-                "p-6 flex gap-4 items-start bg-muted"
+                "p-6 flex gap-4 items-start bg-muted",
             )}
         >
             <div className="w-[28px] flex justify-center items-center bg-primary rounded-md p-1 text-white">
@@ -218,7 +218,7 @@ export default function DocumentIndex({ chat, document }: DocumentIndexProps) {
                                                                     q,
                                                                 )
                                                             }
-                                                            className="col border rounded-lg p-4 hover:bg-gray-300 flex items-center text-left relative"
+                                                            className="col border rounded-lg p-4 hover:bg-gray-300 flex items-center text-left"
                                                         >
                                                             <div className="flex-1">
                                                                 <p className="font-bold">
@@ -233,9 +233,6 @@ export default function DocumentIndex({ chat, document }: DocumentIndexProps) {
                                                                     )}
                                                                 </p>
                                                             </div>
-                                                            <span className="absolute w-full h-full bg-transparent opacity-0 hover:opacity-100 flex items-center justify-end p-6">
-                                                                <Send />
-                                                            </span>
                                                         </button>
                                                     ),
                                                 )}
