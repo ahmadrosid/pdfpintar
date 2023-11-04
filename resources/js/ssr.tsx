a30229c4c8cc79770a3731235e747c384daae0b5
ahmadrosid <alahmadrosid@gmail.com>
2023-11-04 14:44:09 +0700

    fix: ziggy

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,19 +1,24 @@
-import ReactDOMServer from 'react-dom/server';
-import { createInertiaApp } from '@inertiajs/react';
-import createServer from '@inertiajs/react/server';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import route from '../../vendor/tightenco/ziggy/dist/index.m';
+import ReactDOMServer from "react-dom/server";
+import { createInertiaApp } from "@inertiajs/react";
+import createServer from "@inertiajs/react/server";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import route from "ziggy-js";
 
-const appName = 'Laravel';
+const appName = "Laravel";
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: ReactDOMServer.renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.tsx`,
+                import.meta.glob("./Pages/**/*.tsx")
+            ),
         setup: ({ App, props }) => {
             global.route = (name, params, absolute) =>
+                // @ts-ignore
                 route(name, params, absolute, {
                     // @ts-expect-error
                     ...page.props.ziggy,
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
+            input: "resources/js/app.tsx",
+            ssr: "resources/js/ssr.tsx",
             refresh: true,
         }),
         react(),
     ],
+    resolve: {
+        alias: {
+            "ziggy-js": path.resolve("vendor/tightenco/ziggy"),
+        },
+    },
 });
