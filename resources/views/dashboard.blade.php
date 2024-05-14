729d0f1d9e1a4ec2a49387d8e3c123aae66434bf
ahmadrosid <alahmadrosid@gmail.com>
2024-05-15 01:30:36 +0700

    chore: tidy up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Documents') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="flex justify-between items-center py-5 border-b border-gray-200">
-                    <div class="px-4">
-                        <x-text-input placeholder="Search documents" class="mt-2 block w-full" />
+                <div class="flex justify-between items-center border-b border-gray-200">
+                    <div class="px-4 w-[300px]">
+                        <x-text-input placeholder="Search documents" class="block w-full" />
                     </div>
                     <livewire:upload-document />
                 </div>
--- resources/views/livewire/document-list.blade.php
+++ resources/views/livewire/document-list.blade.php
@@ -1,20 +1,20 @@
 <div>
     @foreach($documents as $document)
-            <div class="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100">
-                <a class="flex-1" href="{{ route('documents.show', $document->id) }}">
-                    <div class="flex gap-2">
-                        <x-icon-file />
-                        {{ $document->file_name }}
-                    </div>
-                </a>
-                <div class="flex gap-3 items-center">
-                    <a href="{{ route('documents.show', $document->id) }}">
-                        {{ $document->created_at->diffForHumans() }}
-                    </a>
-                    <button class="hover:text-rose-500 px-2" wire:click="deleteDocument({{ $document->id }})" wire:loading.attr="disabled">
-                        <x-icon-trash />
-                    </button>
+        <div class="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100">
+            <a class="flex-1" href="{{ route('documents.show', $document->id) }}">
+                <div class="flex gap-2">
+                    <x-icon-file />
+                    {{ $document->file_name }}
                 </div>
+            </a>
+            <div class="flex gap-3 items-center">
+                <a href="{{ route('documents.show', $document->id) }}">
+                    {{ $document->created_at->diffForHumans() }}
+                </a>
+                <button class="hover:text-rose-500 px-2" wire:click="deleteDocument({{ $document->id }})" wire:loading.attr="disabled">
+                    <x-icon-trash />
+                </button>
             </div>
+        </div>
     @endforeach
 </div>
