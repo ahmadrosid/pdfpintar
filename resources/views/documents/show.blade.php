5304a1d730d143afd0d624031b7ddbaf08578cda
ahmadrosid <alahmadrosid@gmail.com>
2024-05-15 01:18:13 +0700

    chore: tidy up

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">
         {{ __('Chat with Document') }}
     </x-slot>
-    <div class="h-[7vh] p-2 py-3 items-center flex justify-between">
+    <div class="h-[6vh] p-2 py-3 items-center flex justify-between pt-4">
         <a href="{{ route('documents.index') }}" class="text-gray-900 hover:text-gray-600 font-semibold">
             <span class="left-arrow">&#x2190;</span> {{ $document->file_name }}
         </a>
--- resources/views/livewire/document-chatbot.blade.php
+++ resources/views/livewire/document-chatbot.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-2 gap-2 h-[93vh] p-2">
+<div class="grid grid-cols-2 gap-2 h-[94vh] p-2">
     <div class="overflow-hidden rounded-md">
         <embed src="{{ asset(Storage::url($this->document->file_id)) }}" width="100%" height="100%" type="application/pdf">
     </div>
