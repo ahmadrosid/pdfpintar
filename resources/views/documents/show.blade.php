70e9df8efa3808a1edfebeb9f71d4783fae62bfe
ahmadrosid <alahmadrosid@gmail.com>
2024-08-03 12:41:53 +0700

    fix: remove unecessary views

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -15,5 +15,10 @@
             </a>
         </div>
     </div>
-    <livewire:document-chatbot :document="$document" />
+    <div class="grid grid-cols-2 gap-2 h-[94vh] p-2">
+        <div class="flex flex-col bg-white h-full overflow-hidden">
+            <div id="pdf-viewer" data-url="{{ asset(Storage::url($document->file_path)) }}"></div>
+        </div>
+        <livewire:chat-interface />
+    </div>
 </x-full-screen-layout>
--- resources/views/livewire/document-chatbot.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="grid grid-cols-2 gap-2 h-[94vh] p-2">
-    <div class="flex flex-col bg-white h-full overflow-hidden">
-        <div id="pdf-viewer" data-url="{{ asset(Storage::url($this->document->file_path)) }}"></div>
-    </div>
-    <livewire:chat-interface />
-</div>
