c61ceee31b15c39ff2ca0b4414fc43f98bb58f80
ahmadrosid <alahmadrosid@gmail.com>
2024-08-03 12:48:36 +0700

    chore: tidy up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,11 @@
 import './bootstrap';
 import './react-app';
+
+document.addEventListener('livewire:load', function() {
+    window.addEventListener('user-message-added', function() {
+        const chatMessages = document.querySelector('.chat-messages');
+        if (chatMessages) {
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+        }
+    });
+});
