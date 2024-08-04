d9665950ed9621089380b38edddb331387608b8d
ahmadrosid <alahmadrosid@gmail.com>
2024-08-04 18:38:42 +0700

    feat: use Storage::url to display the url document

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -18,7 +18,7 @@
         </div>
         <div class="grid grid-cols-2 gap-2 h-[94vh] p-2">
             <div class="flex flex-col bg-white h-full overflow-hidden">
-                <div id="pdf-viewer" data-url="{{ asset(Storage::url($document->file_path)) }}"></div>
+                <div id="pdf-viewer" data-url="{{ Storage::url($document->file_path) }}"></div>
             </div>
             <livewire:chat-interface :document="$document" />
         </div>
