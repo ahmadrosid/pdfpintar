bcbdf03f56e9d85734e07f799542ef723a381c74
ahmadrosid <alahmadrosid@gmail.com>
2024-08-30 09:13:38 +0700

    Tidy Up

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,7 +49,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@ahmadrosid.com') && $this->hasVerifiedEmail();
+        return $this->email == 'hey@ahmadrosid.com';
     }
 
 }
