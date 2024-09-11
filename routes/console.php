aa693f16bed14552ddb12db0eeb360ae5946274b
ahmadrosid <alahmadrosid@gmail.com>
2024-09-11 21:24:33 +0700

    Add database scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('backup:clean')->daily()->at('07:00');
+Schedule::command('backup:run --only-db')->daily()->at('07:30');
