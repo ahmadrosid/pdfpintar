9551e359ed2692ac0762f6ea9e77af93605790b6
ahmadrosid <alahmadrosid@gmail.com>
2024-11-27 01:34:05 +0700

    Tidy Up

--- resources/views/livewire/public-chat-interface.blade.php
+++ resources/views/livewire/public-chat-interface.blade.php
@@ -109,7 +109,7 @@
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7l5 5m0 0l-5 5m5-5H6"></path>
                         </svg>
-                        {{ __('Continue to Document') }}
+                        {{ __('Continue chat with Document') }}
                     </a>
                 </div>
             </div>
