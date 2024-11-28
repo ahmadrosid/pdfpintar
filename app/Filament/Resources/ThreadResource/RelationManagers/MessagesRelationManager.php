ad25c7510e0d50ecd8017367919882d4225ebf33
ahmadrosid <alahmadrosid@gmail.com>
2024-11-28 17:20:52 +0700

    make messages read only

--- app/Filament/Resources/ThreadResource/RelationManagers/MessagesRelationManager.php
+++ app/Filament/Resources/ThreadResource/RelationManagers/MessagesRelationManager.php
@@ -37,11 +37,12 @@ class MessagesRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                // Tables\Actions\CreateAction::make(),
             ])
             ->actions([
                 // Tables\Actions\EditAction::make(),
                 // Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
