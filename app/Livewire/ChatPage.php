9ae81e2b14f7dec594bc071748970da4b59605b3
ahmadrosid <alahmadrosid@gmail.com>
2025-03-08 12:41:37 +0700

    wip

--- app/Livewire/ChatPage.php
+++ app/Livewire/ChatPage.php
@@ -2,31 +2,43 @@
 
 namespace App\Livewire;
 
-use App\Lib\ExcelProcessor;
-use App\Lib\PdfProcessor;
-use App\Lib\WordProcessor;
 use App\Models\Document;
 use App\Models\Thread;
 use App\Models\Message;
-use Livewire\Attributes\Computed;
 use Livewire\Component;
-use OpenAI\Laravel\Facades\OpenAI;
-use Livewire\Attributes\On;
-use Throwable;
-use Illuminate\Support\Str;
 
 class ChatPage extends Component
 {
     public Document $document;
 
+    protected $messages = [];
+
+    public function mount()
+    {
+        $existingThread = Thread::where('document_id', $this->document->id)->orderBy('created_at', 'desc')->first();
+        if (!$existingThread) {
+            return;
+        }
+
+        $dbMessages = Message::where('thread_id', $existingThread->id)->orderBy('id', 'asc')->get();
+        foreach ($dbMessages as $message) {
+            $this->messages[] = [
+                'role' => $message->role,
+                'content' => $message->content,
+            ];
+        }
+    }
+
     public function render(): string
     {
         $document = e(json_encode($this->document));
+        $messages = e(json_encode($this->messages));
         return <<<HTML
         <div class="relative flex flex-col border border-neutral-200 bg-white dark:bg-neutral-800 dark:border-neutral-700 h-full max-h-[93vh]">
             <div
                 data-svelte="ChatPage.svelte"
                 data-document="$document"
+                data-messages="$messages"
             ></div>
         </div>
         HTML;
--- resources/js/components/ChatPage.svelte
+++ resources/js/components/ChatPage.svelte
@@ -1,5 +1,6 @@
 <script>
     import ChatInput from "$lib/components/ChatInput.svelte";
+    import Markdown from "$lib/components/Markdown.svelte";
 
     let { wire, dataset } = $props();
     let text = $state('');
@@ -9,11 +10,21 @@
 
 <div class="h-screen max-h-[92vh] relative isolate flex flex-col flex-grow">
     <div class="p-2 border-b border-neutral-200 dark:border-neutral-700">
-        This is the chat page! {dataset.document.file_name}
+        {dataset.document.file_name}
     </div>
 
-    <div class="p-4 mb-2 flex-1">
-        Chat body
+    <div class="mb-2 flex-1">
+        {#each dataset.messages as message}
+            <div class="bg-white p-4 {message.role === 'assistant' ? 'dark:bg-neutral-800' : 'dark:bg-neutral-700'}">
+                <p class="pb-4 text-sm font-medium {message.role === 'assistant' ? 'text-teal-500 dark:text-teal-400' : 'text-orange-500 dark:text-orange-400'}">
+                    {message.role === 'assistant' ? 'pdfpintar' : 'You'}
+                </p>
+
+                <div class="prose dark:prose-invert max-w-none">
+                    <Markdown md={message.content}/>
+                </div>
+            </div>
+        {/each}
     </div>
 
     <ChatInput bind:text {sendMessage} isLoading={false}/>
